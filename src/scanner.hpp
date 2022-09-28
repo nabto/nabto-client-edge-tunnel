57ae1d2525447cdd229c465b031b0f060f88ff75
fusionfoxy <thom2287@gmail.com>
2022-09-28 06:41:22 +0200

    show friendly name from mdns result

--- src/scanner.hpp
+++ src/scanner.hpp
@@ -1,23 +1,24 @@
 #pragma once
 
 #include <nabto_client.hpp>
+#include <3rdparty/nlohmann/json.hpp>
 
 #include <vector>
 #include <chrono>
 #include <thread>
 #include <memory>
 #include <set>
-
+#include <iostream>
 namespace nabto {
 namespace examples {
 namespace common {
 
 class Scanner {
  public:
-    static std::vector<std::tuple<std::string,std::string> > scan(std::shared_ptr<nabto::client::Context> ctx, std::chrono::milliseconds timeout, std::string subtype = "") {
+    static std::vector<std::tuple<std::string,std::string,std::string> > scan(std::shared_ptr<nabto::client::Context> ctx, std::chrono::milliseconds timeout, std::string subtype = "") {
 
         // We put the found devices into a set such that we can remove ipv4/ipv6 duplicates.
-        std::set<std::tuple<std::string, std::string> > localDevices;
+        std::set<std::tuple<std::string, std::string, std::string> > localDevices;
         auto mdnsResolver = ctx->createMdnsResolver(subtype);
 
         std::thread t([mdnsResolver, timeout]() { std::this_thread::sleep_for(timeout); mdnsResolver->stop(); });
@@ -28,7 +29,14 @@ class Scanner {
                 auto result = next->waitForResult();
                 std::string productId = result->getProductId();
                 std::string deviceId = result->getDeviceId();
-                localDevices.insert(std::make_tuple(productId, deviceId));
+                std::string txtItemsStr = result->getTxtItems();
+                nlohmann::json txtItems = nlohmann::json::parse(txtItemsStr);
+                std::cout << "txtItems: " << txtItemsStr << std::endl;
+                std::string fn;
+                try {
+                    fn = txtItems["fn"].get<std::string>();
+                } catch (std::exception& e) { }
+                localDevices.insert(std::make_tuple(productId, deviceId, fn));
             }
         } catch (...) {
 
@@ -36,7 +44,7 @@ class Scanner {
 
         t.join();
 
-        std::vector<std::tuple<std::string, std::string> > ret;
+        std::vector<std::tuple<std::string, std::string, std::string> > ret;
         for (auto d : localDevices) {
             ret.push_back(d);
         }
